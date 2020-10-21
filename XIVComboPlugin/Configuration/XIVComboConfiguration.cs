f4fd0da775ed88778bb52f713c69d3d013c0dd69
Raymond Lynch <raymond.lynch@ironnetcybersecurity.com>
2020-10-21 08:39:40 -0400

    Config cleaning

--- XIVComboPlugin/Configuration/XIVComboConfiguration.cs
+++ XIVComboPlugin/Configuration/XIVComboConfiguration.cs
@@ -22,19 +22,27 @@ namespace XIVComboPlugin
         public bool IsHidden(CustomComboPreset preset) => HiddenActions.Contains(preset);
 
         #region Obsolete
+#pragma warning disable IDE1006 // Naming Styles
 
         // By omitting the getter, we can prevent any obsolete fields from being serialized after the upgrade
 
-        [Obsolete]  // Version < 3
         [JsonProperty("ComboPresets")]
+        [Obsolete("This was removed in favor of EnabledActions in version 4")]
         public LegacyCustomComboPreset _ComboPresets { set { _ComboPresetsBacker = value; } }
+
+        [JsonIgnore]
+        [Obsolete("This was added to prevent serialization of another obsolete property")]
         private LegacyCustomComboPreset _ComboPresetsBacker;
 
-        [Obsolete]  // Version == 3
         [JsonProperty("HiddenActions")]
+        [Obsolete("This was removed in favor of HiddenActions in version 4")]
         public List<bool> _HiddenActions { set { _HiddenActionsBacker = value; } }
+        
+        [JsonIgnore]
+        [Obsolete("This was added to prevent serialization of another obsolete property")]
         private List<bool> _HiddenActionsBacker = new List<bool>();
 
+#pragma warning restore IDE1006 // Naming Styles
         #endregion
 
         public void Upgrade()
@@ -45,8 +53,17 @@ namespace XIVComboPlugin
                 UpgradeToVersion4();
         }
 
+        private void UpgradeToVersion3()
+        {
+            PluginLog.Information("Upgrading configuration to version 3");
+            foreach (var _ in Enum.GetValues(typeof(CustomComboPreset)))
+                _HiddenActionsBacker.Add(false);
+            Version = 3;
+        }
+
         private void UpgradeToVersion4()
         {
+#pragma warning disable CS0612 // Type or member is obsolete
             PluginLog.Information("Upgrading configuration to version 4");
             foreach (LegacyCustomComboPreset legacyPreset in Enum.GetValues(typeof(LegacyCustomComboPreset)))
             {
@@ -65,14 +82,7 @@ namespace XIVComboPlugin
             _ComboPresetsBacker = 0;
             _HiddenActionsBacker = null;
             Version = 4;
-        }
-
-        private void UpgradeToVersion3()
-        {
-            PluginLog.Information("Upgrading configuration to version 3");
-            foreach (var _ in Enum.GetValues(typeof(CustomComboPreset)))
-                _HiddenActionsBacker.Add(false);
-            Version = 3;
+#pragma warning restore CS0612 // Type or member is obsolete
         }
     }
 }
