f166193663e3872077bfb248936d871058044ea1
Grammernatzi <ultragamer2007@gmail.com>
2021-02-27 04:05:18 -0800

    Ruin IV now checks for egi type via enkindle, disabled if Garuda is out, and disabled for Egi Assault I if Titan is out

--- XIVComboPlugin/IconReplacer.cs
+++ XIVComboPlugin/IconReplacer.cs
@@ -1205,6 +1205,9 @@ namespace XIVComboExpandedestPlugin
             {
                 if ((actionID == SMN.EgiAssault || actionID == SMN.EgiAssault2) && HasBuff(SMN.Buffs.FurtherRuin) && PlayerBuffStacks(SMN.Buffs.FurtherRuin) == 4)
                 {
+                    var enkindle = GetIconHook.Original(self, SMN.Enkindle);
+                    if (enkindle == SMN.AerialBlast || (enkindle == SMN.EarthenFury && actionID == SMN.EgiAssault))
+                        return GetIconHook.Original(self, actionID);
                     return SMN.RuinIV;
                 }
             }
