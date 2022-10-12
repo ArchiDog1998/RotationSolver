adc291cedfd5aa500981c02ab712c485718a98a9
Gamous <gammarayburst@163.com>
2022-10-13 05:41:12 +0800

    汉化身位显示, 修复绘图问题, 添加Yaml文件

--- XIVAutoAttack/Actions/EnemyLocation.cs
+++ XIVAutoAttack/Actions/EnemyLocation.cs
@@ -13,4 +13,16 @@ namespace XIVAutoAttack.Actions
         Side,
         Front,
     }
+    public static class EnemyLocationExtensions
+    {
+        public static string ToName(this EnemyLocation value)
+            => value switch
+            {
+                EnemyLocation.None  => "无",
+                EnemyLocation.Back  => "背面",
+                EnemyLocation.Side  => "侧面",
+                EnemyLocation.Front => "正面",
+                _ => "错误",
+            };
+    }
 }
