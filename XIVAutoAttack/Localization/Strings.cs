376e74dc23405e0ec498ba7c9c0b5a02ba837ca5
fatinghenji <37131177+fatinghenji@users.noreply.github.com>
2022-11-27 21:06:42 +0800

    继续更新翻译文本

--- XIVAutoAttack/Localization/Strings.cs
+++ XIVAutoAttack/Localization/Strings.cs
@@ -59,24 +59,23 @@ namespace XIVAutoAttack.Localization
         public string Configwindow_HelpItem_Move { get; set; }
             = "Open one window period to use move skill.";
 
-        public string Configwindow_Helper_SwitchAuthor { get; set; } = "点击以切换作者";
-        public string Configwindow_Helper_GameVersion { get; set; } = "游戏版本";
-        public string Configwindow_Helper_EditCombo { get; set; } = "点击以编辑该自定义Combo。";
-        public string Configwindow_Helper_AddCombo { get; set; } = "添加一个自定义Combo。";
-        public string Configwindow_Helper_OpenSource { get; set; } = "打开源码网址";
-        public string Configwindow_Helper_RunCommand { get; set; } = "单击以执行命令";
-        public string Configwindow_Helper_InsertCommand { get; set; } = "5s内最高优先插入\"{0}\"";
-        public string Configwindow_AttackItem_Description { get; set; } = "你可以选择开启想要的职业的连续GCD战技、技能，若职业与当前职业相同则有命令宏提示。";
-
-        public string Configwindow_AttackItem_ScriptFolder { get; set; } = "自定义循环路径";
-        public string Configwindow_AttackItem_LoadScript { get; set; } = "点击以载入自定义循环";
-        public string Configwindow_AttackItem_ScriptFolderError { get; set; } = "请设定一个路径以正常使用自定义循环！";
-        public string Configwindow_AttackItem_KeyName { get; set; } = "关键名称为";
-        public string Configwindow_Events_AddEvent{ get; set; } = "添加事件";
-        public string Configwindow_Events_Description{ get; set; } = "在这个窗口，你可以设定一些技能释放后，使用什么宏。";
-        public string Configwindow_Events_ActionName{ get; set; } = "技能名称";
-        public string Configwindow_Events_MacroIndex{ get; set; } = "宏编号";
-        public string Configwindow_Events_ShareMacro{ get; set; } = "共享宏";
-        public string Configwindow_Events_RemoveEvent{ get; set; } = "删除事件";
+        public string Configwindow_Helper_SwitchAuthor { get; set; } = "Click to switch authors";
+        public string Configwindow_Helper_GameVersion { get; set; } = "Game Version";
+        public string Configwindow_Helper_EditCombo { get; set; } = "Click to edit this custom Combo.";
+        public string Configwindow_Helper_AddCombo { get; set; } = "Add a custom Combo.";
+        public string Configwindow_Helper_OpenSource { get; set; } = "Open the source code URL";
+        public string Configwindow_Helper_RunCommand { get; set; } = "Click to execute the command";
+        public string Configwindow_Helper_InsertCommand { get; set; } = "Top priority insertion within 5s \"{0}\"";
+        public string Configwindow_AttackItem_Description { get; set; } = "You can choose to turn on continuous GCDs, 0GCDs for the desired profession.\r\nThe occupation in the settings is the same as the current in-game occupation, then there will be a reminder to set the corresponding macro.";
+        public string Configwindow_AttackItem_ScriptFolder { get; set; } = "Custom Loop Storage Path";
+        public string Configwindow_AttackItem_LoadScript { get; set; } = "Click to load a custom loop";
+        public string Configwindow_AttackItem_ScriptFolderError { get; set; } = "Please set a storage path to use the custom loop properly!";
+        public string Configwindow_AttackItem_KeyName { get; set; } = "The key name is";
+        public string Configwindow_Events_AddEvent{ get; set; } = "Adding Events";
+        public string Configwindow_Events_Description{ get; set; } = "In this window, you can set what macros to use after some skills are released.";
+        public string Configwindow_Events_ActionName{ get; set; } = "Skill Name";
+        public string Configwindow_Events_MacroIndex{ get; set; } = "Macro No.";
+        public string Configwindow_Events_ShareMacro{ get; set; } = "Shared Macro No.";
+        public string Configwindow_Events_RemoveEvent{ get; set; } = "Delete Event";
     }
 }
