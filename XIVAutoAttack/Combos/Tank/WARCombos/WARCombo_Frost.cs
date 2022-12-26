c03079e4ef4c01aa744bc1d048894a3b05c37f0b
EFrostBlade <3171264475@qq.com>
2022-12-26 17:59:49 +0800

    优化了解放的释放条件，添加了一个新的战士循环

---
+++ XIVAutoAttack/Combos/Tank/WARCombos/WARCombo_Frost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVAutoAttack_CN.Combos.Tank.WARCombos
+{
+    internal class WARCombo_Frost
+    {
+    }
+}
