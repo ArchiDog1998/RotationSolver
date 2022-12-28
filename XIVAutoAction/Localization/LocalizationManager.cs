14bb3775e4be71b1e63ee26c454f9bbb68b0cf51
ResView <lyyxwangrunze@163.com>
2022-12-28 16:24:55 +0800

    Fix unchanged Localization path

--- XIVAutoAction/Localization/LocalizationManager.cs
+++ XIVAutoAction/Localization/LocalizationManager.cs
@@ -27,8 +27,8 @@ internal class LocalizationManager : IDisposable
 
     private void ReadFile(string lang, Assembly assembly)
     {
-        Stream manifestResourceStream = assembly.GetManifestResourceStream("XIVAutoAttack.Localization." + lang + ".json")
-            ?? assembly.GetManifestResourceStream("XIVAutoAttack_CN.Localization." + lang + ".json");
+        Stream manifestResourceStream = assembly.GetManifestResourceStream("XIVAutoAction.Localization." + lang + ".json")
+            ?? assembly.GetManifestResourceStream("XIVAutoAction_CN.Localization." + lang + ".json");
         if (manifestResourceStream == null) return;
         using StreamReader streamReader = new StreamReader(manifestResourceStream);
         _translations[lang] = JsonConvert.DeserializeObject<Strings>(streamReader.ReadToEnd());
