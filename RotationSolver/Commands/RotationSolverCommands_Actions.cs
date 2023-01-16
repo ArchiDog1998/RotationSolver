7042ef22a5ec2ac9d5a293cd22dab112b13e3553
秋水 <1123993881@qq.com>
2023-01-16 22:43:04 +0800

    remove gif.

--- RotationSolver/Commands/RotationSolverCommands_Actions.cs
+++ RotationSolver/Commands/RotationSolverCommands_Actions.cs
@@ -5,11 +5,7 @@ using RotationSolver.Helpers;
 using RotationSolver.SigReplacers;
 using RotationSolver.Updaters;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RotationSolver.Commands
 {
