8b96ff0bc5a96dd3ba9ad3aba48577cb9fa5edac
秋水 <1123993881@qq.com>
2023-03-01 13:35:46 +0800

    fix: add grade 7 tinctures.

--- RotationSolver/Rotations/CustomRotation/CustomRotation_Tinctures.cs
+++ RotationSolver/Rotations/CustomRotation/CustomRotation_Tinctures.cs
@@ -9,7 +9,12 @@ internal abstract partial class CustomRotation
         TinctureofStrength6 = new BaseItem(36109, 196625),
         TinctureofDexterity6 = new BaseItem(36110, 65535),
         TinctureofMind6 = new BaseItem(36113, 65535),
-        TinctureofIntelligence6 = new BaseItem(36112, 65535);
+        TinctureofIntelligence6 = new BaseItem(36112, 65535),
+
+        TinctureofStrength7 = new BaseItem(37840, 196625),
+        TinctureofDexterity7 = new BaseItem(37841, 65535),
+        TinctureofMind7 = new BaseItem(37843, 65535),
+        TinctureofIntelligence7 = new BaseItem(37844, 65535);
 
     protected bool UseTincture(out IAction act)
     {
@@ -23,18 +28,22 @@ internal abstract partial class CustomRotation
         {
             case JobRole.Tank:
             case JobRole.Melee:
+                if (TinctureofStrength7.CanUse(out act)) return true;
                 if (TinctureofStrength6.CanUse(out act)) return true;
                 break;
 
             case JobRole.RangedPhysical:
+                if (TinctureofDexterity7.CanUse(out act)) return true;
                 if (TinctureofDexterity6.CanUse(out act)) return true;
                 break;
 
             case JobRole.RangedMagicial:
+                if (TinctureofIntelligence7.CanUse(out act)) return true;
                 if (TinctureofIntelligence6.CanUse(out act)) return true;
                 break;
 
             case JobRole.Healer:
+                if (TinctureofMind7.CanUse(out act)) return true;
                 if (TinctureofMind6.CanUse(out act)) return true;
                 break;
         }
