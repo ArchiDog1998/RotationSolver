435ee3589b77e8cbc4da5dd84a6d2bc82444b07c
秋水 <1123993881@qq.com>
2023-04-08 14:11:39 +0800

    style: fixed download counts.

--- UpdateDownloads.ts
+++ UpdateDownloads.ts
@@ -16,17 +16,19 @@ const clearText = (str) => {
 const output = await Promise.all(repos.map(async (repo) => {
   const res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/latest`);
     const data = await res.json();
+    const reses = await fetch(`https://api.github.com/repos/${user}/${repo}/releases`);
+    const datas = await reses.json();
 
     var count = 0;
-    for (var i = 0; i < data.assets.length; i++) {
-        count = count + Number(data.assets[i].download_count);
+    for (var i = 0; i < datas.length; i++) {
+        count = count + Number(datas[i].assets[0].download_count);
     }
 
   const base = {
     AssemblyVersion: data.tag_name.replace(/^v/, ""),
-    Changelog: clearText(data.body),
-    DownloadCount: data.assets[0].download_count,
-    LastUpdate: new Date(data.published_at).valueOf() / 1000,
+      Changelog: clearText(data.body),
+      DownloadCount: count.toString(),
+    LastUpdate: data.published_at,
     DownloadLinkInstall: data.assets[0].browser_download_url,
     DownloadLinkUpdate: data.assets[0].browser_download_url,
   };
