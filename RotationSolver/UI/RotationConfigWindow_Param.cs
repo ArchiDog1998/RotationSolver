af271c03ea57f6763d2036a3b42e1b6ea1d2c724
秋水 <1123993881@qq.com>
2023-04-11 10:37:56 +0800

    fix: add an save to Settings Command Checkbox.

--- RotationSolver/UI/RotationConfigWindow_Param.cs
+++ RotationSolver/UI/RotationConfigWindow_Param.cs
@@ -495,6 +495,7 @@ internal partial class RotationConfigWindow
         {
             Service.Config.SetValue(command, value);
             otherThing?.Invoke();
+            Service.Config.Save();
         });
 
         ImGui.SameLine();
