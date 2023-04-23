0d71ade3178b90448e385f6f26f6821b9bd04c3b
秋水 <1123993881@qq.com>
2023-04-23 22:35:43 +0800

    fix: remove status check.

--- RotationSolver.Basic/Helpers/StatusHelper.cs
+++ RotationSolver.Basic/Helpers/StatusHelper.cs
@@ -189,7 +189,6 @@ public static class StatusHelper
     {
         if (!status.CanDispel()) return false;
         if (status.StackCount > 2) return true;
-        if (status.RemainingTime > 8 + DataCenter.WeaponRemain) return true;
         return dangerousStatus.Any(id => (uint)id == status.StatusId);
     }
 
