ff22a3d95dce4f497a8702be87360691c4f79236
Riot <edu.buntu.unix@gmail.com>
2023-04-27 07:17:58 +0200

    feat: added integer getter/setter for rotation config.
    Relying on float for whole numbers is not pretty, so here's an integer option.

---
+++ RotationSolver.Basic/Configuration/RotationConfig/RotationConfigInt.cs
@@ -0,0 +1,13 @@
+﻿namespace RotationSolver.Basic.Configuration.RotationConfig;
+
+public class RotationConfigInt : RotationConfigBase
+{
+    public int Min, Max, Speed;
+
+    public RotationConfigInt(string name, int value, string displayName, int min, int max, int speed) : base(name, value.ToString(), displayName)
+    {
+        Min = min;
+        Max = max;
+        Speed = speed;
+    }
+}
