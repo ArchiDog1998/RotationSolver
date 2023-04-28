0f696b7d1010af13e4ba907463c7002ca6af871e
秋水 <1123993881@qq.com>
2023-04-28 16:18:15 +0800

    fix: changed warning in High-end duty.

--- RotationSolver/Localization/Strings.cs
+++ RotationSolver/Localization/Strings.cs
@@ -592,6 +592,6 @@ internal partial class Strings
         #endregion        
     };
 
-    public string HighEndWarning { get; set; } = "You'd better not use Rotation Solver in {0}!";
+    public string HighEndWarning { get; set; } = "Please separately keybind damage reduction / shield cooldowns in case RS fails at a crucial moment in {0}!";
     public string HighEndBan { get; set; } = "{0} can not be used in High-end Duty!";
 }
