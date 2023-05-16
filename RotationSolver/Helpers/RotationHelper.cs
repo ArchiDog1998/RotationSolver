a1cce78ec0a092c0b41251c2dfe8031c65d8b915
Riot <edu.buntu.unix@gmail.com>
2023-05-16 14:12:56 +0200

    fix: Another go at RotationHelper

--- RotationSolver/Helpers/RotationHelper.cs
+++ RotationSolver/Helpers/RotationHelper.cs
@@ -9,11 +9,28 @@ internal record AssemblyInfo(string Name, string Author, string Path, string Sup
 
 internal static class RotationHelper
 {
-    public static List<LoadedAssembly> LoadedCustomRotations = new();
+    private static readonly Dictionary<Assembly, AssemblyInfo> _assemblyInfos = new();
 
-    public static readonly Dictionary<Assembly, AssemblyInfo> _assemblyInfos = new();
+    public static List<LoadedAssembly> LoadedCustomRotations { get; } = new List<LoadedAssembly>();
 
     public static string[] AllowedAssembly { get; private set; } = Array.Empty<string>();
+
+    public static async Task LoadListAsync()
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync("https://raw.githubusercontent.com/ArchiDog1998/RotationSolver/main/Resources/whitelist.json");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            AllowedAssembly = JsonConvert.DeserializeObject<string[]>(content);
+        }
+        catch (Exception ex)
+        {
+            PluginLog.Log(ex, "Failed to load white List.");
+        }
+    }
+
     public static AssemblyInfo GetInfo(this Assembly assembly)
     {
         if (_assemblyInfos.TryGetValue(assembly, out var info))
@@ -36,38 +53,13 @@ internal static class RotationHelper
         return assemblyInfo;
     }
 
-
-    public static async Task LoadListAsync()
-    {
-        try
-        {
-            using var client = new HttpClient();
-            var response = await client.GetAsync("https://raw.githubusercontent.com/ArchiDog1998/RotationSolver/main/Resources/whitelist.json");
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
-            AllowedAssembly = JsonConvert.DeserializeObject<string[]>(content);
-        }
-        catch (Exception ex)
-        {
-            PluginLog.Log(ex, "Failed to load white List.");
-        }
-    }
-
     public static bool IsAllowed(this ICustomRotation rotation, out string name)
     {
         name = "Unknown";
         if (rotation == null) return false;
 
-        Assembly assembly;
-        try
-        {
-            assembly = rotation.GetType().Assembly;
-        }
-        catch (Exception ex)
-        {
-            PluginLog.LogError($"Failed to get assembly for rotation {rotation.GetType().Name}: {ex}");
-            return false;
-        }
+        var assembly = GetTypeAssembly(rotation);
+        if (assembly == null) return false;
 
         name = assembly.GetName().Name;
         return assembly.IsAllowed();
@@ -77,10 +69,23 @@ internal static class RotationHelper
     {
         if (_assemblyInfos.TryGetValue(assembly, out var info))
         {
-            var assemblyName = info.Name + " - " + info.Author;
+            var assemblyName = $"{info.Name} - {info.Author}";
             return AllowedAssembly.Contains(assemblyName);
         }
         return false;
+    }
+
+    public static Assembly GetTypeAssembly(this ICustomRotation rotation)
+    {
+        try
+        {
+            return rotation.GetType().Assembly;
+        }
+        catch (Exception ex)
+        {
+            PluginLog.LogError($"Failed to get assembly for rotation {rotation.GetType().Name}: {ex}");
+            return null;
+        }
     }
 
     public static Vector4 GetColor(this ICustomRotation rotation)
@@ -111,28 +116,43 @@ internal static class RotationHelper
 
     public static Assembly LoadCustomRotationAssembly(string filePath)
     {
-        var loadContext = new RotationLoadContext(new FileInfo(filePath).Directory);
+        var directoryInfo = new FileInfo(filePath).Directory;
+        var loadContext = new RotationLoadContext(directoryInfo);
         var assembly = loadContext.LoadFromFile(filePath);
-        var assemblyName = assembly.GetName().Name;
-        var existingAssembly = GetAssemblyFromPath(filePath);
 
+        var assemblyName = assembly.GetName().Name;
+        var author = GetAuthor(filePath, assemblyName);
+
+        var attr = assembly.GetCustomAttribute<AssemblyLinkAttribute>();
+        var assemblyInfo = new AssemblyInfo(
+            assemblyName,
+            author,
+            filePath,
+            attr?.SupportLink,
+            attr?.HelpLink,
+            attr?.ChangeLog,
+            attr?.Donate);
+
+        var existingAssembly = GetAssemblyFromPath(filePath);
         if (existingAssembly != null)
         {
             _assemblyInfos.Remove(existingAssembly);
         }
 
-        var author = GetAuthor(filePath, assemblyName);
-        var attr = assembly.GetCustomAttribute<AssemblyLinkAttribute>();
-        var assemblyInfo = new AssemblyInfo(assemblyName, author, filePath, attr?.SupportLink, attr?.HelpLink, attr?.ChangeLog, attr?.Donate);
         _assemblyInfos[assembly] = assemblyInfo;
 
-        var loadedAssembly = new LoadedAssembly { Path = filePath, LastModified = File.GetLastWriteTimeUtc(filePath).ToString() };
-        var existingLoadedAssemblyIndex = LoadedCustomRotations.FindIndex(item => item.Path == loadedAssembly.Path);
-        if (existingLoadedAssemblyIndex != -1) LoadedCustomRotations.RemoveAt(existingLoadedAssemblyIndex);
+        var loadedAssembly = new LoadedAssembly
+        {
+            Path = filePath,
+            LastModified = File.GetLastWriteTimeUtc(filePath).ToString()
+        };
+
+        LoadedCustomRotations.RemoveAll(item => item.Path == loadedAssembly.Path);
         LoadedCustomRotations.Add(loadedAssembly);
 
         return assembly;
     }
+
 
     private static Assembly GetAssemblyFromPath(string filePath)
     {
@@ -151,4 +171,6 @@ internal static class RotationHelper
         var fileVersionInfo = FileVersionInfo.GetVersionInfo(filePath);
         return string.IsNullOrWhiteSpace(fileVersionInfo.CompanyName) ? assemblyName : fileVersionInfo.CompanyName;
     }
+
+
 }
