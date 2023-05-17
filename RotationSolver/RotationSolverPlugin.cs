3ead70c57f7a9931686acc7d02db9bb1e9eb0f0a
秋水 <1123993881@qq.com>
2023-05-18 00:11:12 +0800

    fix: add ECommons real!

--- RotationSolver/RotationSolverPlugin.cs
+++ RotationSolver/RotationSolverPlugin.cs
@@ -1,7 +1,9 @@
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
 using Dalamud.Interface.Windowing;
+using Dalamud.Logging;
 using Dalamud.Plugin;
+using ECommons;
 using RotationSolver.Basic.Configuration;
 using RotationSolver.Commands;
 using RotationSolver.Localization;
@@ -26,6 +28,8 @@ public sealed class RotationSolverPlugin : IDalamudPlugin, IDisposable
     public unsafe RotationSolverPlugin(DalamudPluginInterface pluginInterface)
     {
         pluginInterface.Create<Service>();
+
+        ECommonsMain.Init(pluginInterface, this, ECommons.Module.SplatoonAPI);
 
         try
         {
