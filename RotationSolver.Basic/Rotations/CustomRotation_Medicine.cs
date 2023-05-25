84065f306a267fd36d64c0179e2f83e4ba50d65b
秋水 <1123993881@qq.com>
2023-05-25 15:41:03 +0800

    fix: add grade 8 tinctures.

--- RotationSolver.Basic/Rotations/CustomRotation_Medicine.cs
+++ RotationSolver.Basic/Rotations/CustomRotation_Medicine.cs
@@ -21,8 +21,18 @@ public abstract partial class CustomRotation
     public static IBaseItem TinctureOfMind7 { get; }
         = new MedicineItem(37844, MedicineType.Mind);
 
+    public static IBaseItem TinctureOfStrength8 { get; }
+    = new MedicineItem(39727, MedicineType.Strength);
+    public static IBaseItem TinctureOfDexterity8 { get; }
+        = new MedicineItem(39728, MedicineType.Dexterity);
+    public static IBaseItem TinctureOfIntelligence8 { get; }
+        = new MedicineItem(39730, MedicineType.Intelligence);
+    public static IBaseItem TinctureOfMind8 { get; }
+        = new MedicineItem(39731, MedicineType.Mind);
+
     static bool UseStrength(out IAction act)
     {
+        if (TinctureOfStrength8.CanUse(out act)) return true;
         if (TinctureOfStrength7.CanUse(out act)) return true;
         if (TinctureOfStrength6.CanUse(out act)) return true;
         return false;
@@ -30,18 +40,21 @@ public abstract partial class CustomRotation
 
     static bool UseDexterity(out IAction act)
     {
+        if (TinctureOfDexterity8.CanUse(out act)) return true;
         if (TinctureOfDexterity7.CanUse(out act)) return true;
         if (TinctureOfDexterity6.CanUse(out act)) return true;
         return false;
     }
     static bool UseIntelligence(out IAction act)
     {
+        if (TinctureOfIntelligence8.CanUse(out act)) return true;
         if (TinctureOfIntelligence7.CanUse(out act)) return true;
         if (TinctureOfIntelligence6.CanUse(out act)) return true;
         return false;
     }
     static bool UseMind(out IAction act)
     {
+        if (TinctureOfMind8.CanUse(out act)) return true;
         if (TinctureOfMind7.CanUse(out act)) return true;
         if (TinctureOfMind6.CanUse(out act)) return true;
         return false;
