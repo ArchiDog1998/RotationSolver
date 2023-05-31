16359fe48bb7bc6ca372283e2a132d3f1aa8a9be
秋水 <1123993881@qq.com>
2023-05-31 23:23:31 +0800

    fix: add XIVPainter for future using.

---
+++ RotationSolver/UI/PainterManager.cs
@@ -0,0 +1,24 @@
+﻿using ECommons.DalamudServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotationSolver.UI;
+
+internal static class PainterManager
+{
+    static XIVPainter.XIVPainter _painter;
+
+
+    public static void Init()
+    {
+        _painter = Svc.PluginInterface.Create<XIVPainter.XIVPainter>("RotationSolverOverlay");
+    }
+
+    public static void Dispose()
+    {
+        _painter?.Dispose();
+    }
+}
