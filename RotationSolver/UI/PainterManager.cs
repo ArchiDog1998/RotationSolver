fa01bbc9f3c655ea548d0e177b9f6f635842eb0b
秋水 <1123993881@qq.com>
2023-06-02 15:47:58 +0800

    fix: patch painter.

--- RotationSolver/UI/PainterManager.cs
+++ RotationSolver/UI/PainterManager.cs
@@ -7,7 +7,7 @@ namespace RotationSolver.UI;
 internal static class PainterManager
 {
     static XIVPainter.XIVPainter _painter;
-    static Drawing3DCircularSectorFO _noneCir, _flankCir1, _flankCir2, _rearCir;
+    static Drawing3DCircularSectorFO _noneCir, _flankCir, _rearCir;
     static Drawing3DAnnulusFO _annulus;
 
     public static void Init()
@@ -19,12 +19,13 @@ internal static class PainterManager
         var wrong = ImGui.ColorConvertFloat4ToU32(new Vector4(0.3f, 0.8f, 0.2f, 0.15f));
 
         _noneCir = new Drawing3DCircularSectorFO(null, 3, wrong, 2);
-        _flankCir1 = new Drawing3DCircularSectorFO(null, 3, wrong, 2, MathF.PI * 0.25f, MathF.PI / 2);
-        _flankCir2 = new Drawing3DCircularSectorFO(null, 3, wrong, 2, MathF.PI * 1.25f, MathF.PI / 2);
-        _rearCir = new Drawing3DCircularSectorFO(null, 3, wrong, 2, MathF.PI * 0.75f, MathF.PI / 2);
 
-        _noneCir.InsideColor = _flankCir1.InsideColor = _flankCir2.InsideColor
-            = _rearCir.InsideColor = right;
+        _flankCir = new Drawing3DCircularSectorFO(null, 3, wrong, 2, XIVPainter.Enum.RadiusInclude.IncludeBoth,
+            new Vector2( MathF.PI * 0.25f, MathF.PI / 2), new Vector2(MathF.PI * 1.25f, MathF.PI / 2));
+        _rearCir = new Drawing3DCircularSectorFO(null, 3, wrong, 2, XIVPainter.Enum.RadiusInclude.IncludeBoth,
+            new Vector2(MathF.PI * 0.75f, MathF.PI / 2));
+
+        _noneCir.InsideColor = _flankCir.InsideColor = _rearCir.InsideColor = right;
 
         _annulus = new Drawing3DAnnulusFO(null, 3, 3 + Service.Config.MeleeRangeOffset, right, 2);
         _annulus.InsideColor = ImGui.ColorConvertFloat4ToU32(new Vector4(0.8f, 0.3f, 0.2f, 0.15f));
@@ -42,7 +43,7 @@ internal static class PainterManager
             }
         };
 
-        _painter.AddDrawings(_noneCir, _flankCir1, _flankCir2, _rearCir, _annulus);
+        _painter.AddDrawings(_noneCir, _flankCir, _rearCir, _annulus);
 
         if (Player.Available)
         {
@@ -64,19 +65,19 @@ internal static class PainterManager
         switch(positional)
         {
             case EnemyPositional.Flank:
-                _flankCir1.Target = _flankCir2.Target = target;
+                _flankCir.Target = target;
                 _rearCir.Target = null;
                 _noneCir.Target = null;
                 break;
 
             case EnemyPositional.Rear:
-                _flankCir1.Target = _flankCir2.Target = null;
+                _flankCir.Target = null;
                 _rearCir.Target = target;
                 _noneCir.Target = null;
                 break;
 
             default:
-                _flankCir1.Target = _flankCir2.Target = null;
+                _flankCir.Target = null;
                 _rearCir.Target = null;
                 _noneCir.Target = target;
                 break;
@@ -85,7 +86,7 @@ internal static class PainterManager
 
     public static void ClearPositional()
     {
-        _flankCir1.Target = _flankCir2.Target = null;
+        _flankCir.Target = null;
         _rearCir.Target = null;
         _noneCir.Target = null;
     }
