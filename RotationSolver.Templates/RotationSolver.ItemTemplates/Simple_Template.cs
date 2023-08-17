c04296ea2baa552a433623bec302efd4c81e0882
Molionr Shio <Molionr@outlook.com>
2023-08-17 15:07:26 +0800

    Updating the Rotation Solver template

--- RotationSolver.Templates/RotationSolver.ItemTemplates/Simple_Template.cs
+++ RotationSolver.Templates/RotationSolver.ItemTemplates/Simple_Template.cs
@@ -1,14 +1,23 @@
 ﻿namespace RotationSolver.ItemTemplates;
 
-[LinkDescription("$Your link description here, it is better to link to a png! this attribute can be multiple! $")]
-[SourceCode("$If your rotation is open source, please write the link to this attribute! $")]
+// RotationDesc here is used to display burst actions, indicating that if 'Auto Burst' is not turned on, the actions will not be released
+[RotationDesc(ActionID.Lightspeed)]
+// Link to the Ratation source code
+[SourceCode(Path = "main/DefaultRotations/Melee/NIN_Default.cs")]
+// The detailed or extended description links of this Ratation, such as loop diagrams, recipe urls, teaching videos, etc.,
+// can be written more than one
+[LinkDescription("https://www.thebalanceffxiv.com/img/jobs/nin/earlymug3.png")]
+[LinkDescription("https://www.thebalanceffxiv.com/img/jobs/nin/nininfographicwindows.png")]
+[LinkDescription("https://docs.google.com/spreadsheets/u/0/d/1BZZrqWMRrugCeiBICEgjCz2vRNXt_lRTxPnSQr24Em0/htmlview#",
+    "Under the “Planner (With sample section)”")]
 
 // Change this base class to your job's base class. It is named like XXX_Base.
 public class Simple_Template : AST_Base 
 {
     //Change this to the game version right now.
-    public override string GameVersion => "6.38";
+    public override string GameVersion => "6.48";
 
+    // Change this to your custom Rotation name
     public override string RotationName => "$safeitemname$";
 
     //GCD actions here.
--- RotationSolver.Templates/RotationSolver.ProjectTemplate/AssemblyInfo.cs
+++ RotationSolver.Templates/RotationSolver.ProjectTemplate/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿//Put your hash into this one if you want someone say hello to you when they are using RotationSolver in Duty with you.
-[assembly: AuthorHash("$Your Hash in Debug Tab$")]
+[assembly: AuthorHash(Hash = "Ig4lHXUohMZNIeheUtAtRg==")]
 
 //Something to show your link.
-[assembly: AssemblyLink("$Support(Forum) Link$", "$Help(wiki) Link$",
-    "$ChangeLog$", "$Donate Link$")]
+[assembly: AssemblyLink(Donate = "https://ko-fi.com/archited", UserName = "ArchiDog1998", Repository = "FFXIVRotations")]
