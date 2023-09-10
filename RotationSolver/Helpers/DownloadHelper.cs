610972afbf34f7e74aa7bf58174dfb5df0840f27
秋水 <1123993881@qq.com>
2023-09-10 11:38:18 +0800

    fix: add contributor's say hello.

--- RotationSolver/Helpers/DownloadHelper.cs
+++ RotationSolver/Helpers/DownloadHelper.cs
@@ -6,13 +6,16 @@ namespace RotationSolver.Helpers;
 
 public static class DownloadHelper
 {
-    public static string[] LinkLibraries { get; private set; }
-    public static IncompatiblePlugin[] IncompatiblePlugins { get; private set; }
+    public static string[] LinkLibraries { get; private set; } = Array.Empty<string>();
+    public static string[] ContributorsHash { get; private set; } = Array.Empty<string>();
+    public static IncompatiblePlugin[] IncompatiblePlugins { get; private set; } = Array.Empty<IncompatiblePlugin>();
 
     public static async Task DownloadAsync()
     {
-        LinkLibraries = await DownloadOneAsync<string[]>($"https://raw.githubusercontent.com/{Service.USERNAME}/{Service.REPO}/main/Resources/downloadList.json");
-        IncompatiblePlugins = await DownloadOneAsync<IncompatiblePlugin[]>($"https://raw.githubusercontent.com/{Service.USERNAME}/{Service.REPO}/main/Resources/IncompatiblePlugins.json");
+        LinkLibraries = await DownloadOneAsync<string[]>($"https://raw.githubusercontent.com/{Service.USERNAME}/{Service.REPO}/main/Resources/downloadList.json") ?? Array.Empty<string>();
+        IncompatiblePlugins = await DownloadOneAsync<IncompatiblePlugin[]>($"https://raw.githubusercontent.com/{Service.USERNAME}/{Service.REPO}/main/Resources/IncompatiblePlugins.json") ?? Array.Empty<IncompatiblePlugin>();
+
+        ContributorsHash = await DownloadOneAsync<string[]>($"https://raw.githubusercontent.com/{Service.USERNAME}/{Service.REPO}/main/Resources/ContributorsHash.json") ?? Array.Empty<string>();
     }
 
     private static async Task<T> DownloadOneAsync<T>(string url)
