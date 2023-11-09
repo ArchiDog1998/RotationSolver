73d10dbd09e0e949be69fa23b579b752fbdb92f8
秋水 <1123993881@qq.com>
2023-11-09 17:36:15 +0800

    docs: remove the tag about `PvE`.

--- RotationSolver/Commands/RSCommands_Actions.cs
+++ RotationSolver/Commands/RSCommands_Actions.cs
@@ -8,229 +8,228 @@ using RotationSolver.Localization;
 using RotationSolver.UI;
 using RotationSolver.Updaters;
 
-namespace RotationSolver.Commands
+namespace RotationSolver.Commands;
+
+public static partial class RSCommands
 {
-    public static partial class RSCommands
+    static DateTime _lastClickTime = DateTime.MinValue;
+    static bool _lastState;
+
+    internal static unsafe bool CanDoAnAction(bool isGCD)
     {
-        static DateTime _lastClickTime = DateTime.MinValue;
-        static bool _lastState;
-
-        internal static unsafe bool CanDoAnAction(bool isGCD)
+        if (!_lastState || !DataCenter.State)
         {
-            if (!_lastState || !DataCenter.State)
-            {
-                _lastState = DataCenter.State;
-                return false;
-            }
             _lastState = DataCenter.State;
-
-            if (!Player.Available) return false;
-
-            //Do not click the button in random time.
-            if (DateTime.Now - _lastClickTime < TimeSpan.FromMilliseconds(new Random().Next(
-                (int)(Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin) * 1000), (int)(Service.Config.GetValue(PluginConfigFloat.ClickingDelayMax) * 1000)))) return false;
-            _lastClickTime = DateTime.Now;
-
-            if (!isGCD && ActionUpdater.NextAction is IBaseAction act1 && act1.IsRealGCD) return false;
-
-            return true;
+            return false;
         }
-        internal static DateTime _lastUsedTime = DateTime.MinValue;
-        internal static uint _lastActionID;
-        public static void DoAction()
+        _lastState = DataCenter.State;
+
+        if (!Player.Available) return false;
+
+        //Do not click the button in random time.
+        if (DateTime.Now - _lastClickTime < TimeSpan.FromMilliseconds(new Random().Next(
+            (int)(Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin) * 1000), (int)(Service.Config.GetValue(PluginConfigFloat.ClickingDelayMax) * 1000)))) return false;
+        _lastClickTime = DateTime.Now;
+
+        if (!isGCD && ActionUpdater.NextAction is IBaseAction act1 && act1.IsRealGCD) return false;
+
+        return true;
+    }
+    internal static DateTime _lastUsedTime = DateTime.MinValue;
+    internal static uint _lastActionID;
+    public static void DoAction()
+    {
+        var wrong = new Random().NextDouble() < Service.Config.GetValue(PluginConfigFloat.MistakeRatio) && ActionUpdater.WrongAction != null;
+        var nextAction = wrong ? ActionUpdater.WrongAction : ActionUpdater.NextAction;
+        if (nextAction == null) return;
+
+        if (wrong)
         {
-            var wrong = new Random().NextDouble() < Service.Config.GetValue(PluginConfigFloat.MistakeRatio) && ActionUpdater.WrongAction != null;
-            var nextAction = wrong ? ActionUpdater.WrongAction : ActionUpdater.NextAction;
-            if (nextAction == null) return;
+            Svc.Toasts.ShowError(string.Format(LocalizationManager.RightLang.ClickingMistakeMessage, nextAction));
+            ControlWindow.Wrong = nextAction;
+            ControlWindow.DidTime = DateTime.Now;
+        }
 
-            if (wrong)
+        if (nextAction is BaseAction act1 && act1.IsPvP && !act1.IsFriendly
+            && !act1.IsTargetArea
+            && act1.Target is PlayerCharacter p/* && p != Player.Object*/)
+        {
+            var hash = SocialUpdater.EncryptString(p);
+
+            //Don't attack authors and contributors!!
+            if (RotationUpdater.AuthorHashes.ContainsKey(hash)
+                || DownloadHelper.ContributorsHash.Contains(hash))
             {
-                Svc.Toasts.ShowError(string.Format(LocalizationManager.RightLang.ClickingMistakeMessage, nextAction));
-                ControlWindow.Wrong = nextAction;
-                ControlWindow.DidTime = DateTime.Now;
+                Svc.Chat.PrintError($"Please don't attack RS developers with RS by {act1}!");
+                return;
             }
-
-            if (nextAction is BaseAction act1 && act1.IsPvP && !act1.IsFriendly
-                && !act1.IsTargetArea
-                && act1.Target is PlayerCharacter p/* && p != Player.Object*/)
-            {
-                var hash = SocialUpdater.EncryptString(p);
-
-                //Don't attack authors and contributors!!
-                if (RotationUpdater.AuthorHashes.ContainsKey(hash)
-                    || DownloadHelper.ContributorsHash.Contains(hash))
-                {
-                    Svc.Chat.PrintError($"Please don't attack RS developers with RS by {act1}!");
-                    return;
-                }
-            }
+        }
 
 #if DEBUG
-            //if (nextAction is BaseAction acti)
-            //    Svc.Log.Debug($"Will Do {acti}");
+        //if (nextAction is BaseAction acti)
+        //    Svc.Log.Debug($"Will Do {acti}");
 #endif
 
-            if (Service.Config.GetValue(PluginConfigBool.KeyBoardNoise))
+        if (Service.Config.GetValue(PluginConfigBool.KeyBoardNoise))
+        {
+            PreviewUpdater.PulseActionBar(nextAction.AdjustedID);
+        }
+
+        if (nextAction.Use())
+        {
+            OtherConfiguration.RotationSolverRecord.ClickingCount++;
+
+            _lastActionID = nextAction.AdjustedID;
+            _lastUsedTime = DateTime.Now;
+
+            if (nextAction is BaseAction act)
             {
-                PreviewUpdater.PulseActionBar(nextAction.AdjustedID);
-            }
+                if (Service.Config.GetValue(PluginConfigBool.KeyBoardNoise))
+                    Task.Run(() => PulseSimulation(nextAction.AdjustedID));
 
-            if (nextAction.Use())
-            {
-                OtherConfiguration.RotationSolverRecord.ClickingCount++;
-
-                _lastActionID = nextAction.AdjustedID;
-                _lastUsedTime = DateTime.Now;
-
-                if (nextAction is BaseAction act)
-                {
-                    if (Service.Config.GetValue(PluginConfigBool.KeyBoardNoise))
-                        Task.Run(() => PulseSimulation(nextAction.AdjustedID));
-
-                    if (act.ShouldEndSpecial) ResetSpecial();
+                if (act.ShouldEndSpecial) ResetSpecial();
 #if DEBUG
-                    //Svc.Chat.Print(act.Name);
-                    //Svc.Chat.Print(act.Target?.Name.TextValue ?? string.Empty);
-                    //foreach (var item in act.AffectedTargets)
-                    //{
-                    //    Svc.Chat.Print(item?.Name.TextValue ?? string.Empty);
-                    //}
+                //Svc.Chat.Print(act.Name);
+                //Svc.Chat.Print(act.Target?.Name.TextValue ?? string.Empty);
+                //foreach (var item in act.AffectedTargets)
+                //{
+                //    Svc.Chat.Print(item?.Name.TextValue ?? string.Empty);
+                //}
 #endif
-                    //Change Target
-                    var tar = (act.Target == null || act.Target == Player.Object)
-                        ? act.AffectedTargets.FirstOrDefault() : act.Target;
+                //Change Target
+                var tar = (act.Target == null || act.Target == Player.Object)
+                    ? act.AffectedTargets.FirstOrDefault() : act.Target;
 
-                    if (tar != null && tar != Player.Object && tar.IsEnemy())
+                if (tar != null && tar != Player.Object && tar.IsEnemy())
+                {
+                    DataCenter.HostileTarget = tar;
+                    if (!DataCenter.IsManual
+                        && (Service.Config.GetValue(PluginConfigBool.SwitchTargetFriendly) || ((Svc.Targets.Target?.IsEnemy() ?? true)
+                        || Svc.Targets.Target?.GetObjectKind() == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.Treasure)))
                     {
-                        DataCenter.HostileTarget = tar;
-                        if (!DataCenter.IsManual
-                            && (Service.Config.GetValue(PluginConfigBool.SwitchTargetFriendly) || ((Svc.Targets.Target?.IsEnemy() ?? true)
-                            || Svc.Targets.Target?.GetObjectKind() == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.Treasure)))
-                        {
-                            Svc.Targets.Target = tar;
-                        }
+                        Svc.Targets.Target = tar;
                     }
                 }
             }
         }
+    }
 
-        static bool started = false;
-        static async void PulseSimulation(uint id)
+    static bool started = false;
+    static async void PulseSimulation(uint id)
+    {
+        if (started) return;
+        started = true;
+        try
         {
-            if (started) return;
-            started = true;
-            try
+            for (int i = 0; i < new Random().Next(Service.Config.GetValue(PluginConfigInt.KeyBoardNoiseMin),
+                Service.Config.GetValue(PluginConfigInt.KeyBoardNoiseMax)); i++)
             {
-                for (int i = 0; i < new Random().Next(Service.Config.GetValue(PluginConfigInt.KeyBoardNoiseMin),
-                    Service.Config.GetValue(PluginConfigInt.KeyBoardNoiseMax)); i++)
-                {
-                    PreviewUpdater.PulseActionBar(id);
-                    var time = Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin) +
-                        new Random().NextDouble() * (Service.Config.GetValue(PluginConfigFloat.ClickingDelayMax) - Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin));
-                    await Task.Delay((int)(time * 1000));
-                }
+                PreviewUpdater.PulseActionBar(id);
+                var time = Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin) +
+                    new Random().NextDouble() * (Service.Config.GetValue(PluginConfigFloat.ClickingDelayMax) - Service.Config.GetValue(PluginConfigFloat.ClickingDelayMin));
+                await Task.Delay((int)(time * 1000));
             }
-            catch (Exception ex)
-            {
-                Svc.Log.Warning(ex, "Pulse Failed!");
-            }
-            finally { started = false; }
-            started = false;
+        }
+        catch (Exception ex)
+        {
+            Svc.Log.Warning(ex, "Pulse Failed!");
+        }
+        finally { started = false; }
+        started = false;
+    }
+
+    internal static void ResetSpecial()
+    {
+        DoSpecialCommandType(SpecialCommandType.EndSpecial, false);
+    }
+    internal static void CancelState()
+    {
+        if (DataCenter.State) DoStateCommandType(StateCommandType.Cancel);
+    }
+
+    static float _lastCountdownTime = 0;
+    internal static void UpdateRotationState()
+    {
+        if (ActionUpdater.AutoCancelTime != DateTime.MinValue &&
+            (!DataCenter.State || DataCenter.InCombat))
+        {
+            ActionUpdater.AutoCancelTime = DateTime.MinValue;
         }
 
-        internal static void ResetSpecial()
+        var target = DataCenter.AllHostileTargets.FirstOrDefault(t => t.TargetObjectId == Player.Object.ObjectId);
+
+        if (Svc.Condition[ConditionFlag.LoggingOut])
         {
-            DoSpecialCommandType(SpecialCommandType.EndSpecial, false);
+            CancelState();
         }
-        internal static void CancelState()
+        else if (Service.Config.GetValue(PluginConfigBool.AutoOffWhenDead)
+            && Player.Available
+            && Player.Object.CurrentHp == 0)
         {
-            if (DataCenter.State) DoStateCommandType(StateCommandType.Cancel);
+            CancelState();
+        }
+        else if (Service.Config.GetValue(PluginConfigBool.AutoOffCutScene)
+            && Svc.Condition[ConditionFlag.OccupiedInCutSceneEvent])
+        {
+            CancelState();
+        }
+        else if (Service.Config.GetValue(PluginConfigBool.AutoOffBetweenArea)
+            && (Svc.Condition[ConditionFlag.BetweenAreas]
+            || Svc.Condition[ConditionFlag.BetweenAreas51]))
+        {
+            CancelState();
+        }
+        //Cancel when pull
+        else if (Service.CountDownTime == 0 && _lastCountdownTime > 0.2f)
+        {
+            _lastCountdownTime = 0;
+            CancelState();
+        }
+        //Auto manual on being attacked by someone.
+        else if (Service.Config.GetValue(PluginConfigBool.StartOnAttackedBySomeone)
+            && target != null
+            && !target.IsDummy())
+        {
+            if (!DataCenter.State)
+            {
+                DoStateCommandType(StateCommandType.Manual);
+            }
+        }
+        //Auto start at count Down.
+        else if (Service.Config.GetValue(PluginConfigBool.StartOnCountdown)
+            && Service.CountDownTime > 0)
+        {
+            _lastCountdownTime = Service.CountDownTime;
+            if (!DataCenter.State)
+            {
+                DoStateCommandType(StateCommandType.Auto);
+            }
+        }
+        //Cancel when after combat.
+        else if (ActionUpdater.AutoCancelTime != DateTime.MinValue
+            && DateTime.Now > ActionUpdater.AutoCancelTime)
+        {
+            CancelState();
+            ActionUpdater.AutoCancelTime = DateTime.MinValue;
         }
 
-        static float _lastCountdownTime = 0;
-        internal static void UpdateRotationState()
+        //Auto switch conditions.
+        else if (DataCenter.RightSet.SwitchCancelConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
         {
-            if (ActionUpdater.AutoCancelTime != DateTime.MinValue &&
-                (!DataCenter.State || DataCenter.InCombat))
+            CancelState();
+        }
+        else if (DataCenter.RightSet.SwitchManualConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
+        {
+            if (!DataCenter.State)
             {
-                ActionUpdater.AutoCancelTime = DateTime.MinValue;
+                DoStateCommandType(StateCommandType.Manual);
             }
-
-            var target = DataCenter.AllHostileTargets.FirstOrDefault(t => t.TargetObjectId == Player.Object.ObjectId);
-
-            if (Svc.Condition[ConditionFlag.LoggingOut])
+        }
+        else if (DataCenter.RightSet.SwitchAutoConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
+        {
+            if (!DataCenter.State)
             {
-                CancelState();
-            }
-            else if (Service.Config.GetValue(PluginConfigBool.AutoOffWhenDead)
-                && Player.Available
-                && Player.Object.CurrentHp == 0)
-            {
-                CancelState();
-            }
-            else if (Service.Config.GetValue(PluginConfigBool.AutoOffCutScene)
-                && Svc.Condition[ConditionFlag.OccupiedInCutSceneEvent])
-            {
-                CancelState();
-            }
-            else if (Service.Config.GetValue(PluginConfigBool.AutoOffBetweenArea)
-                && (Svc.Condition[ConditionFlag.BetweenAreas]
-                || Svc.Condition[ConditionFlag.BetweenAreas51]))
-            {
-                CancelState();
-            }
-            //Cancel when pull
-            else if (Service.CountDownTime == 0 && _lastCountdownTime > 0.2f)
-            {
-                _lastCountdownTime = 0;
-                CancelState();
-            }
-            //Auto manual on being attacked by someone.
-            else if (Service.Config.GetValue(PluginConfigBool.StartOnAttackedBySomeone)
-                && target != null
-                && !target.IsDummy())
-            {
-                if (!DataCenter.State)
-                {
-                    DoStateCommandType(StateCommandType.Manual);
-                }
-            }
-            //Auto start at count Down.
-            else if (Service.Config.GetValue(PluginConfigBool.StartOnCountdown)
-                && Service.CountDownTime > 0)
-            {
-                _lastCountdownTime = Service.CountDownTime;
-                if (!DataCenter.State)
-                {
-                    DoStateCommandType(StateCommandType.Auto);
-                }
-            }
-            //Cancel when after combat.
-            else if (ActionUpdater.AutoCancelTime != DateTime.MinValue
-                && DateTime.Now > ActionUpdater.AutoCancelTime)
-            {
-                CancelState();
-                ActionUpdater.AutoCancelTime = DateTime.MinValue;
-            }
-
-            //Auto switch conditions.
-            else if (DataCenter.RightSet.SwitchCancelConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
-            {
-                CancelState();
-            }
-            else if (DataCenter.RightSet.SwitchManualConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
-            {
-                if (!DataCenter.State)
-                {
-                    DoStateCommandType(StateCommandType.Manual);
-                }
-            }
-            else if (DataCenter.RightSet.SwitchAutoConditionSet?.IsTrue(DataCenter.RightNowRotation) ?? false)
-            {
-                if (!DataCenter.State)
-                {
-                    DoStateCommandType(StateCommandType.Auto);
-                }
+                DoStateCommandType(StateCommandType.Auto);
             }
         }
     }
