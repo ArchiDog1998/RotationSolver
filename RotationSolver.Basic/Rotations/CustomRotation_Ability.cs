da03a858415b4160d134a00b08fb2e9621aceafe
Ehl'tee Es <127939494+LTS-FFXIV@users.noreply.github.com>
2024-05-15 23:52:32 -0500

    Revert "Sprint Penalty Fix for Deep Dungeon"

--- RotationSolver.Basic/Rotations/CustomRotation_Ability.cs
+++ RotationSolver.Basic/Rotations/CustomRotation_Ability.cs
@@ -371,10 +371,7 @@ partial class CustomRotation
         if (SprintPvP.CanUse(out act)) return true;
 
         if (PelotonPvE.CanUse(out act, skipAoeCheck: true)) return true;
-        if (!Player.HasStatus(false, StatusID.SprintPenalty))
-        {
-            if (SprintPvE.CanUse(out act)) return true;
-        }
+        if (SprintPvE.CanUse(out act)) return true;
 
         if (DataCenter.RightNowDutyRotation?.SpeedAbility(nextGCD, out act) ?? false) return true;
         return false;
