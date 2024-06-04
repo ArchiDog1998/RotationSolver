38c9a78f592041f9db17117a7a991671ed276b68
秋水 <1123993881@qq.com>
2024-06-04 11:24:30 +0800

    fix: timeline auto heal or auto defense fix.

--- RotationSolver/Helpers/RotationHelper.cs
+++ RotationSolver/Helpers/RotationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Dalamud.Interface.Colors;
+using ECommons.DalamudServices;
 using RotationSolver.Data;
 using System.Diagnostics;
 
@@ -9,6 +10,13 @@ internal static class RotationHelper
     private static readonly Dictionary<Assembly, AssemblyInfo> _assemblyInfos = [];
 
     public static List<LoadedAssembly> LoadedCustomRotations { get; } = [];
+
+    private static readonly Version? RsVersion = GetRSBasicVersion(typeof(RotationHelper).Assembly);
+
+    public static Version? GetRSBasicVersion(Assembly assembly)
+    {
+        return assembly.GetReferencedAssemblies().Where(n => n.Name == "RotationSolver.Basic").FirstOrDefault()?.Version;
+    }
 
     public static AssemblyInfo GetInfo(this Assembly assembly)
     {
@@ -59,6 +67,23 @@ internal static class RotationHelper
         var loadContext = new RotationLoadContext(directoryInfo);
         var assembly = loadContext.LoadFromFile(filePath);
 
+        var existingAssembly = GetAssemblyFromPath(filePath);
+
+        var version = GetRSBasicVersion(assembly);
+        if (version is not null && RsVersion is not null)
+        {
+            if(version.Major < RsVersion.Major
+                || version.Minor < RsVersion.Minor
+                || version.Build < RsVersion.Build)
+            {
+                Svc.Log.Error($"The assembly from \"{filePath}\" may be too old, please update it.");
+                if (existingAssembly != null)
+                {
+                    return existingAssembly;
+                }
+            }
+        }
+
         var assemblyName = assembly.GetName().Name;
         var author = GetAuthor(filePath, assemblyName);
 
@@ -72,7 +97,6 @@ internal static class RotationHelper
             link?.Repository,
             DateTime.Now);
 
-        var existingAssembly = GetAssemblyFromPath(filePath);
         if (existingAssembly != null)
         {
             _assemblyInfos.Remove(existingAssembly);
