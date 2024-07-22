c8fd283760ee96b775b6ed1cd260f841b2e7515b
LTS <127939494+LTS-FFXIV@users.noreply.github.com>
2024-07-22 13:27:45 -0500

    GNB base update (tested to 90, no bricking anymore), needs further tests and changes to rotation logic post 90

--- RotationSolver.Basic/Rotations/Basic/GunbreakerRotation.cs
+++ RotationSolver.Basic/Rotations/Basic/GunbreakerRotation.cs
@@ -1,4 +1,4 @@
-﻿namespace RotationSolver.Basic.Rotations.Basic;
+namespace RotationSolver.Basic.Rotations.Basic;
 
 partial class GunbreakerRotation
 {
@@ -33,130 +33,28 @@ partial class GunbreakerRotation
     public static byte MaxAmmo => CartridgeChargeIiTrait.EnoughLevel ? (byte)3 : (byte)2;
     #endregion
 
-    static partial void ModifyBurstStrikePvE(ref ActionSetting setting)
+    static partial void ModifyKeenEdgePvE(ref ActionSetting setting)
     {
-        setting.ActionCheck = () => Ammo > 0;
-    }
 
-    static partial void ModifyGnashingFangPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => AmmoComboStep == 0 && Ammo > 0;
     }
-
-    static partial void ModifySavageClawPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.GnashingFangPvE) == ActionID.SavageClawPvE;
-    }
-
-    static partial void ModifyWickedTalonPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.GnashingFangPvE) == ActionID.WickedTalonPvE;
-    }
-
-    static partial void ModifyJugularRipPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.JugularRipPvE;
-    }
-
-    static partial void ModifyAbdomenTearPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.AbdomenTearPvE;
-    }
-
-    static partial void ModifyEyeGougePvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.EyeGougePvE;
-    }
-
-    static partial void ModifyHypervelocityPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.HypervelocityPvE;
-    }
-
-    static partial void ModifyLightningShotPvE(ref ActionSetting setting)
-    {
-        setting.SpecialType = SpecialActionType.MeleeRange;
-    }
-
-    static partial void ModifyReignOfBeastsPvE(ref ActionSetting setting)
-    {
-        setting.StatusNeed = [StatusID.ReadyToReign];
-    }
-
-    static partial void ModifyNobleBloodPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ReignOfBeastsPvE) == ActionID.NobleBloodPvE;
-        setting.ComboIds = [ActionID.ReignOfBeastsPvE];
-    }
-
-    static partial void ModifyLionHeartPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ReignOfBeastsPvE) == ActionID.LionHeartPvE;
-        setting.ComboIds = [ActionID.NobleBloodPvE];
-    }
-
-    static partial void ModifyFatedCirclePvE(ref ActionSetting setting)
-    {
-        setting.StatusProvide = [ StatusID.ReadyToRaze ];
-        setting.ActionCheck = () => Ammo > 0;
-    }
-
-    static partial void ModifyDoubleDownPvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = () => Ammo > 1;
-    }
-
-    static partial void ModifyAuroraPvE(ref ActionSetting setting)
-    {
-        setting.TargetStatusProvide = [StatusID.Aurora];
-    }
-
-    static partial void ModifyNebulaPvE(ref ActionSetting setting)
-    {
-        setting.StatusProvide = StatusHelper.RampartStatus;
-        setting.ActionCheck = Player.IsTargetOnSelf;
-    }
-
-    static partial void ModifyGreatNebulaPvE(ref ActionSetting setting)
-    {
-        setting.StatusProvide = StatusHelper.RampartStatus;
-        setting.ActionCheck = Player.IsTargetOnSelf;
-    }
-
-    static partial void ModifyCamouflagePvE(ref ActionSetting setting)
-    {
-        setting.ActionCheck = Player.IsTargetOnSelf;
-    }
-
-    static partial void ModifySonicBreakPvE(ref ActionSetting setting)
-    {
-        setting.StatusNeed = [ StatusID.ReadyToBreak ];
-    }
-
-    private protected sealed override IBaseAction TankStance => RoyalGuardPvE;
 
     static partial void ModifyNoMercyPvE(ref ActionSetting setting)
     {
-        setting.StatusProvide = [ StatusID.ReadyToBreak ];
+        setting.StatusProvide = [StatusID.ReadyToBreak];
         setting.CreateConfig = () => new()
         {
             TimeToKill = 10,
         };
     }
 
-    static partial void ModifyBloodfestPvE(ref ActionSetting setting)
+    static partial void ModifyBrutalShellPvE(ref ActionSetting setting)
     {
-        setting.StatusProvide = [StatusID.ReadyToReign];
-        setting.ActionCheck = () => MaxAmmo - Ammo > 1;
+        setting.ComboIds = [ActionID.KeenEdgePvE];
     }
 
-    static partial void ModifyFatedBrandPvE(ref ActionSetting setting)
+    static partial void ModifyCamouflagePvE(ref ActionSetting setting)
     {
-        setting.StatusNeed = [StatusID.ReadyToRaze];
-        setting.CreateConfig = () => new ActionConfig()
-        {
-            AoeCount = 2,
-        };
+        setting.ActionCheck = () => Player.IsTargetOnSelf();
     }
 
     static partial void ModifyDemonSlicePvE(ref ActionSetting setting)
@@ -167,12 +65,59 @@ partial class GunbreakerRotation
         };
     }
 
+    private protected sealed override IBaseAction TankStance => RoyalGuardPvE;
+
+    static partial void ModifyLightningShotPvE(ref ActionSetting setting)
+    {
+        setting.SpecialType = SpecialActionType.MeleeRange;
+    }
+
+    static partial void ModifyDangerZonePvE(ref ActionSetting setting)
+    {
+        
+    }
+
+    static partial void ModifySolidBarrelPvE(ref ActionSetting setting)
+    {
+        setting.ComboIds = [ActionID.BrutalShellPvE];
+    }
+
+    static partial void ModifyBurstStrikePvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.ReadyToBlast, StatusID.ReadyToBlast_3041];
+        setting.ActionCheck = () => Ammo > 0;
+    }
+
+    static partial void ModifyNebulaPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = StatusHelper.RampartStatus;
+        setting.ActionCheck = () => Player.IsTargetOnSelf();
+    }
+
     static partial void ModifyDemonSlaughterPvE(ref ActionSetting setting)
     {
+        setting.ComboIds = [ActionID.DemonSlicePvE];
         setting.CreateConfig = () => new()
         {
             AoeCount = 2,
         };
+    }
+
+    static partial void ModifyAuroraPvE(ref ActionSetting setting)
+    {
+        setting.TargetStatusProvide = [StatusID.Aurora];
+    }
+
+    static partial void ModifySuperbolidePvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.Superbolide];
+        setting.ActionCheck = () => Player.IsTargetOnSelf();
+    }
+
+    static partial void ModifySonicBreakPvE(ref ActionSetting setting)
+    {
+        setting.StatusNeed = [StatusID.ReadyToBreak];
+        setting.TargetStatusProvide = [StatusID.SonicBreak];
     }
 
     static partial void ModifyTrajectoryPvE(ref ActionSetting setting)
@@ -180,14 +125,157 @@ partial class GunbreakerRotation
         setting.SpecialType = SpecialActionType.MovingForward;
     }
 
-    static partial void ModifyRoughDividePvP(ref ActionSetting setting)
+    static partial void ModifyGnashingFangPvE(ref ActionSetting setting)
     {
-        setting.SpecialType = SpecialActionType.MovingForward;
+        setting.ActionCheck = () => AmmoComboStep == 0 && Ammo > 0;
+        setting.StatusProvide = [StatusID.ReadyToRip];
+    }
+
+    static partial void ModifySavageClawPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.GnashingFangPvE) == ActionID.SavageClawPvE;
+        setting.ComboIds = [ActionID.GnashingFangPvE];
+        setting.StatusProvide = [StatusID.ReadyToTear];
+    }
+
+    static partial void ModifyWickedTalonPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.GnashingFangPvE) == ActionID.WickedTalonPvE;
+        setting.ComboIds = [ActionID.SavageClawPvE];
+        setting.StatusProvide = [StatusID.ReadyToGouge];
+    }
+
+    static partial void ModifyBowShockPvE(ref ActionSetting setting)
+    {
+        setting.TargetStatusProvide = [StatusID.BowShock];
+    }
+
+    static partial void ModifyHeartOfLightPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.HeartOfLight];
+        setting.ActionCheck = () => Player.IsTargetOnSelf();
+    }
+
+    static partial void ModifyHeartOfStonePvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.HeartOfStone];
+        setting.ActionCheck = () => Player.IsParty() || Player.IsTargetOnSelf();
     }
 
     static partial void ModifyContinuationPvE(ref ActionSetting setting)
     {
         setting.UnlockedByQuestID = 68802;
+    }
+
+    static partial void ModifyJugularRipPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.JugularRipPvE;
+        setting.ComboIds = [ActionID.GnashingFangPvE];
+        setting.StatusNeed = [StatusID.ReadyToRip];
+    }
+
+    static partial void ModifyAbdomenTearPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.AbdomenTearPvE;
+        setting.ComboIds = [ActionID.SavageClawPvE];
+        setting.StatusNeed = [StatusID.ReadyToTear];
+    }
+
+    static partial void ModifyEyeGougePvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.EyeGougePvE;
+        setting.ComboIds = [ActionID.WickedTalonPvE];
+        setting.StatusNeed = [StatusID.ReadyToGouge];
+    }
+
+    static partial void ModifyFatedCirclePvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.ReadyToRaze];
+        setting.ActionCheck = () => Ammo > 0;
+    }
+
+    static partial void ModifyBloodfestPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.ReadyToReign];
+        setting.ActionCheck = () => MaxAmmo - Ammo > 1;
+    }
+
+    static partial void ModifyBlastingZonePvE(ref ActionSetting setting)
+    {
+
+    }
+
+    static partial void ModifyHeartOfCorundumPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = [StatusID.CatharsisOfCorundum, StatusID.ClarityOfCorundum];
+        setting.ActionCheck = () => Player.IsParty() || Player.IsTargetOnSelf();
+    }
+
+    static partial void ModifyHypervelocityPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ContinuationPvE) == ActionID.HypervelocityPvE;
+        setting.ComboIds = [ActionID.BurstStrikePvE];
+        setting.StatusNeed = [StatusID.ReadyToBlast];
+    }
+
+    static partial void ModifyDoubleDownPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Ammo > 1;
+        setting.CreateConfig = () => new()
+        {
+            AoeCount = 1,
+        };
+    }
+
+    static partial void ModifyGreatNebulaPvE(ref ActionSetting setting)
+    {
+        setting.StatusProvide = StatusHelper.RampartStatus;
+        setting.ActionCheck = () => Player.IsTargetOnSelf();
+    }
+
+    static partial void ModifyFatedBrandPvE(ref ActionSetting setting)
+    {
+        setting.ComboIds = [ActionID.FatedCirclePvE];
+        setting.StatusNeed = [StatusID.ReadyToRaze];
+        setting.CreateConfig = () => new ActionConfig()
+        {
+            AoeCount = 2,
+        };
+    }
+
+    static partial void ModifyReignOfBeastsPvE(ref ActionSetting setting)
+    {
+        setting.StatusNeed = [StatusID.ReadyToReign];
+        setting.CreateConfig = () => new ActionConfig()
+        {
+            AoeCount = 1,
+        };
+    }
+
+    static partial void ModifyNobleBloodPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ReignOfBeastsPvE) == ActionID.NobleBloodPvE;
+        setting.ComboIds = [ActionID.ReignOfBeastsPvE];
+        setting.CreateConfig = () => new ActionConfig()
+        {
+            AoeCount = 1,
+        };
+    }
+
+    static partial void ModifyLionHeartPvE(ref ActionSetting setting)
+    {
+        setting.ActionCheck = () => Service.GetAdjustedActionId(ActionID.ReignOfBeastsPvE) == ActionID.LionHeartPvE;
+        setting.ComboIds = [ActionID.NobleBloodPvE];
+        setting.CreateConfig = () => new ActionConfig()
+        {
+            AoeCount = 1,
+        };
+    }
+
+    // PVP
+    static partial void ModifyRoughDividePvP(ref ActionSetting setting)
+    {
+        setting.SpecialType = SpecialActionType.MovingForward;
     }
 
     /// <inheritdoc/>
